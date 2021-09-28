1ec3e33b65aff9dfbb34fc7a399f62b0c483764d
Alexander Melnikov <okman5757@gmail.com>
2021-09-29 02:15:18 +0300

    refactor

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,8 +2,10 @@ import { LoginDTO } from '../../dto/login.dto';
 import { RegistrationDTO } from '../../dto/registration.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from '../../models/user.model';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { compare } from 'bcryptjs';
+import { GenerateResponse } from '../../helpers/generateResponse';
+import { IResponse } from '../../interfaces/IResponse';
 
 @Injectable()
 export class UsersService {
@@ -13,16 +15,34 @@ export class UsersService {
         return await this.userRepository.create(dto);
     }
 
-    async getUserByEmail(email: string): Promise<User> {
+    async getUserByEmail(email: string) {
         const user = await this.userRepository.findOne({ where: { email }, include: { all: true } });
-        if (user) return user;
-        else throw new HttpException(`Пользователь с почтой '${email}' не найден`, HttpStatus.BAD_REQUEST);
+        if (user)
+            return new GenerateResponse({
+                data: { user },
+            }) as IResponse<{ user: User }>;
+        else
+            return new GenerateResponse({
+                status: HttpStatus.BAD_REQUEST,
+                error: true,
+                message: 'Пользователь не найден',
+                data: null,
+            }) as IResponse<null>;
     }
 
-    async getUserById(id: number): Promise<User> {
+    async getUserById(id: number) {
         const user = await this.userRepository.findByPk(id, { include: { all: true } });
-        if (user) return user;
-        else throw new HttpException('Пользователь не найден', HttpStatus.BAD_REQUEST);
+        if (user)
+            return new GenerateResponse({
+                data: { user },
+            }) as IResponse<{ user: User }>;
+        else
+            return new GenerateResponse({
+                status: HttpStatus.BAD_REQUEST,
+                error: true,
+                message: 'Пользователь не найден',
+                data: null,
+            }) as IResponse<null>;
     }
 
     async validateNewUser(email: string): Promise<boolean> {
@@ -30,8 +50,8 @@ export class UsersService {
         return !user;
     }
 
-    async validateUser(dto: LoginDTO): Promise<User> | Promise<null> {
-        const user = await this.getUserByEmail(dto.email);
+    async validateUser(dto: LoginDTO): Promise<User | null> {
+        const user = await this.userRepository.findOne({ where: { email: dto.email } });
         const passwordEquals = await compare(dto.password, user.password);
         if (user && passwordEquals) return user;
         else return null;
