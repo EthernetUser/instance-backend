e742f4ccb88504066116fd79ead10290f8a41c0c
Alexander Melnikov <okman5757@gmail.com>
2021-09-28 19:18:53 +0300

    add methods to comments controller

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { CreateCommentDTO } from '../../dto/createComment.dto';
+import { UpdateCommentDTO } from '../../dto/updateComment.dto';
 
 @Controller('api/comments')
 export class CommentsController {
@@ -8,5 +10,20 @@ export class CommentsController {
     @Get('/:lessonId')
     async getCommentsByLessonId(@Param('lessonId') lessonId: number) {
         return this.commentsService.getCommentsByLessonId(lessonId);
+    }
+
+    @Post('/create')
+    async createComment(@Body() dto: CreateCommentDTO) {
+        return this.commentsService.createComment(dto);
+    }
+
+    @Put('/update')
+    async updateComment(@Body() dto: UpdateCommentDTO) {
+        return this.commentsService.updateComment(dto);
+    }
+
+    @Delete('/delete/:id')
+    async deleteComment(@Param('id') id: number) {
+        return this.commentsService.deleteComment(id);
     }
 }
