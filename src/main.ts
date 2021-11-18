1fbe672cf647d5359f3108dfdfddc7ea49ca10cf
Alexander Melnikov <okman5757@gmail.com>
2021-11-18 15:04:13 +0300

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { AppModule } from './modules/app/app.module';
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './modules/app/app.module';
 
-const start = async () => {
+const bootstrap = async () => {
     try {
         const app = await NestFactory.create<NestExpressApplication>(AppModule);
         const PORT = process.env.PORT || 5000;
@@ -21,4 +21,4 @@ const start = async () => {
     }
 };
 
-start();
+bootstrap();
